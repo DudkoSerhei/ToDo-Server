b32eb38a5b54cb8d05ec5dd0d0fab25c925a0dba
DudkoSerhei <serhei.dudko@gmail.com>
2017-12-01 16:56:51 +0300

    Add fetch request

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from 'http';
 import express from 'express';
+import 'whatwg-fetch';
+import "isomorphic-fetch";
 
 let app = express();
 app.server = http.createServer(app);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+fetch('/api/todos');
 
 export default app;
